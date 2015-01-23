f591d1db93f47e6bbaba7db0afe7d33c10ab981f
Ashok Kumar <ashokslsk@gmail.com>
2015-01-23 15:13:42 +0530

    Apple Top apps RSSFeed XML parser

---
+++ RssFeedDownloader/src/dev/ash/rssfeeddownloader/Applications.java
@@ -0,0 +1,33 @@
+package dev.ash.rssfeeddownloader;
+
+public class Applications {
+
+	private String name;
+	private String artist;
+	private String releaseDate;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public String toString(){
+		return "Name: "+ this.name + "\n" + 
+	           "Artist: " + this.artist + "\n" +
+			   "ReleaseDate: " + this.releaseDate + "\n";
+	}
+	
+}
